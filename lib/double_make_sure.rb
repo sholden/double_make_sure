4fe3e0686a9583b1bc95e77d289200ff96c78ea2
Scott Holden <scott@sshconnection.com>
2012-01-26 17:25:45 -0800

    making sure i pull in dependencies

--- lib/double_make_sure.rb
+++ lib/double_make_sure.rb
@@ -1,8 +1,13 @@
 require "double_make_sure/version"
+require "double_make_sure/version"
 require 'double_make_sure/double_execution'
+require 'double_make_sure/double_execution'
+require 'double_make_sure/double_testing'
 require 'double_make_sure/double_testing'
 
 require 'active_support'
+require 'active_support'
+require 'active_support/test_case'
 require 'active_support/test_case'
 
 module DoubleMakeSure
